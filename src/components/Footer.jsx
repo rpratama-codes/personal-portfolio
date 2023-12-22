44c0b18904e791255f2060ed53eeb392e5500f93
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2023-12-22 16:22:11 +0700

    🔷 update some styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+// import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Footer() {
-	const navigate = useNavigate()
 
 	return (
-		<footer className='container mx-auto h-56 bg-black mt-20 p-20 flex items-center'>
-			<img onClick={()=>navigate("/")} src="/assets/images/logo-white.svg" alt="logo" style={{ height: 84 }} />
+		<footer className='container mx-auto h-56 bg-black mt-20 p-20 flex items-center max-lg:justify-center'>
+			<Link to="/">
+				<img src="/assets/images/logo-white.svg" alt="logo" style={{ height: 84 }} />
+			</Link>
 		</footer>
 	)
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,25 @@
 // import React from 'react'
-import { useNavigate } from "react-router-dom"
+import { Link } from "react-router-dom"
 import * as Icons from 'react-feather'
 
 export default function Navbar() {
-	const navigate = useNavigate()
 
 	return (
 		<nav className='container mx-auto flex justify-between items-center'>
-			<div>
-				<img onClick={() => navigate("/")} className='h-20 min-h-20' src='/assets/images/logo.svg' />
+			<div className="flex items-center gap-10">
+				<Link to="/">
+					<img className='h-20 min-h-20' src='/assets/images/logo.svg' />
+				</Link>
+				<Link to="/"
+					className="max-sm:hidden flex items-center px-4 h-20 border-black hover:bg-black hover:text-white">
+					<p className='font-bold'>Home</p>
+				</Link>
 			</div>
 			<div className='flex'>
-				{/* <div className='flex flex-col h-fit p-4  border-black hover:bg-black hover:text-white hover:cursor-pointer'>
-            <p className=' font-bold text-xl'>Project Gallery</p>
-          </div> */}
 				<a href="https://www.linkedin.com/in/rizqi-pratama" target='_blank' rel="noreferrer">
-					<div className='flex  items-center justify-center h-fit p-4  border-black hover:bg-black hover:text-white gap-3'>
+					<div className='flex  items-center justify-between h-20 px-2  border-black hover:bg-black hover:text-white gap-5'>
 						<Icons.Linkedin />
-						<p className='font-bold '>Connect on Linkedin</p>
+						<p className='font-bold'>Connect on Linkedin</p>
 					</div>
 				</a>
 			</div>
