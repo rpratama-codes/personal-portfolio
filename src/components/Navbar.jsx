c3b9fff2667c75d7216c116147fa6a11a509f689
Rizqi Pratama <35159027+rizqikazukun@users.noreply.github.com>
2023-12-22 16:57:03 +0700

    🚩 fix bug cursor showing on mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ export default function Navbar() {
 		<nav className='container mx-auto flex justify-between items-center'>
 			<div className="flex items-center gap-10">
 				<Link to="/">
-					<img className='h-20 min-h-20' src='/assets/images/logo.svg' />
+					<img className='h-20 min-h-20 flex-shrink-0' src='/assets/images/logo.svg' />
 				</Link>
 				<Link to="/"
 					className="max-sm:hidden flex items-center px-4 h-20 border-black hover:bg-black hover:text-white">
@@ -17,9 +17,13 @@ export default function Navbar() {
 			</div>
 			<div className='flex'>
 				<a href="https://www.linkedin.com/in/rizqi-pratama" target='_blank' rel="noreferrer">
-					<div className='flex  items-center justify-between h-20 px-2  border-black hover:bg-black hover:text-white gap-5'>
+					<div className='flex items-center h-20 px-2 border-black hover:bg-black hover:text-white'>
+						<div className="p-2">
 						<Icons.Linkedin />
-						<p className='font-bold'>Connect on Linkedin</p>
+						</div>
+						<div className="p-2">
+							<p className="font-bold break-word">Connect on Linkedin</p>
+						</div>
 					</div>
 				</a>
 			</div>
