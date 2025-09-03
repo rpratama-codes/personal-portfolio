5aa6fd5554d9c84a940c7b2e8f65946405986bc0
rpratama-codes <35159027+rpratama-codes@users.noreply.github.com>
2025-09-03 09:55:05 +0700

    fixing embeded data

--- src/assets/data.js
+++ src/assets/data.js
@@ -3,7 +3,7 @@ export const data = [
         name: "Mama Recipe & Orenji Backend",
         image: "https://res.cloudinary.com/dwptyupfa/image/upload/v1702826733/personal/cvynrvf04nbw3h1ltxxm.png",
         description: "This is a about Backend service for Mama Recipe and Orenji, both are same backend while Orenji is One of a Mobile version from Mama Recipe Project. This Project is about a recipe applicaion that has a service like adding,save,favorite,like recipe, and other. Note : The ilustration below not an actual data flow, it just an overview from overall business logic.",
-        repo: "https://github.com/rizqikazukun/mama-recipe-web-be",
+        repo: "https://github.com/rpratama-codes/mama-recipe-web-be",
         collection: 'https://documenter.getpostman.com/view/31887036/2s9YkoeMvV',
         stack: ["Javascript", "ExpressJS", "Sequelize", "Postgres", "FCM (Admin)", "Firestore (Admin)", "Smtp", "Cloudinary", "Jsonwebtoken", "cors", "helmet"],
         image_hero: "https://res.cloudinary.com/dwptyupfa/image/upload/v1703063735/article/ygz9ketoei5nspyvjytd.png"
@@ -12,7 +12,7 @@ export const data = [
         name: "Mama Recipe Frontend",
         image: "https://res.cloudinary.com/dwptyupfa/image/upload/v1702814033/personal/ssvt9lrb0rfenjzb5ovu.png",
         description: "This website is a place where you can find and share delicious recipes from various cuisines. You can search for your favorite food, or browse through the popular dishes that are recommended for you. You can also learn how to make nasi goreng, soto ayam, semur kikil, and many other mouth-watering dishes from the comfort of your home. Whether you are a beginner or a master chef, you will find something that suits your taste and skill level. You can also upload your own recipe and show off your culinary talent to the world. Join this website today and eating, cooking, and repeating!",
-        repo: "https://github.com/rizqikazukun/mama-recipe-web-fe",
+        repo: "https://github.com/rpratama-codes/mama-recipe-web-fe",
         link: "https://recipes-web-fe.vercel.app",
         stack: ["javascript", "React", "React Router", "Redux", "Bootstrap"],
         image_hero: 'https://res.cloudinary.com/dwptyupfa/image/upload/v1703065795/article/lbbljpak4of2fsb53nm7.png'
@@ -21,8 +21,8 @@ export const data = [
         name: "Orenji Mobile",
         image: "https://res.cloudinary.com/dwptyupfa/image/upload/v1702814876/personal/bqi2ecb0l1ajzkqxyyji.png",
         description: "Halo selamat datang di project ini, Project ini adalah versi mobile dari web mama recipe, Mama Recipe adalah website untuk melihat, membuat, dan membagikan resep. Existing Feature antara lain login, register, search recipe, save recipe. untuk add recipe saat ini belum tersedia, dan bisa menggunakan web terlebih daulu.",
-        repo: "https://github.com/rizqikazukun/orenji-native-mobile",
-        apk: 'https://github.com/rizqikazukun/orenji-native-mobile/releases/tag/debug-preview',
+        repo: "https://github.com/rpratama-codes/orenji-native-mobile",
+        apk: 'https://github.com/rpratama-codes/orenji-native-mobile/releases/tag/debug-preview',
         stack: ["Javascript", "React Native", "React Native Navigation", "Firebase Cloud Messaging (Client)", "Redux"],
         image_hero: "https://res.cloudinary.com/dwptyupfa/image/upload/v1703065665/article/r8rkiha7nqxylqlpzhrl.png"
     },
@@ -30,7 +30,7 @@ export const data = [
         name: "Peworld Frontend",
         image: "https://res.cloudinary.com/dwptyupfa/image/upload/v1702814032/personal/epusaqfvj9u7utqzopek.png",
         description: "TPeworld Hiring web. learning base project from pijarcamp. Using NextJS Framework as the base of the project. For more Peworld is a hiring app that connects recruiters and job seekers in the industry 4.0. Exsiting Feature are Login, Register, search talent, hiring talent.",
-        repo: "https://github.com/rizqikazukun/peworld-hiring-app",
+        repo: "https://github.com/rpratama-codes/peworld-hiring-app",
         link: "https://peworld-hiring-app.vercel.app",
         stack: ["javascript", "React", "NextJS", "Tailwind", "SASS"],
         image_hero: "https://res.cloudinary.com/dwptyupfa/image/upload/v1703065825/article/tttmrhuomqvm2ylc8bjv.png"
@@ -39,7 +39,7 @@ export const data = [
         name: "Tickitz Backend",
         image: "https://res.cloudinary.com/dwptyupfa/image/upload/v1702826733/personal/glj3io7rq2b0ilbvaj5l.png",
         description: "It's a tickitz backend. learning base project from pijarcamp. This a movie cinema ticketing app, you can see current month movie show, and upcoming movie on next month.",
-        repo: "https://github.com/rizqikazukun/tickitz-be",
+        repo: "https://github.com/rpratama-codes/tickitz-be",
         collection: 'https://documenter.getpostman.com/view/31887036/2s9YkoeMvT',
         stack: ["Javascript", "ExpressJS", "Postgres", "Jsonwebtoken", "cors", "helmet"],
     },
@@ -47,7 +47,7 @@ export const data = [
         name: "Tickitz Frontend",
         image: "https://res.cloudinary.com/dwptyupfa/image/upload/v1702814033/personal/wkxawyb17s7bnfffrr4u.png",
         description: "It's a tickitz frontend. learning base project from pijarcamp. This a movie cinema ticketing app, you can see current month movie show, and upcoming movie on next month. also this project was integrating with midtrans payment gateway on sandbox environment.",
-        repo: "https://github.com/rizqikazukun/tickitz-fe",
+        repo: "https://github.com/rpratama-codes/tickitz-fe",
         link: "https://tickitz-app.vercel.app/",
         stack: ["javascript", "React", "React Router", "Bootstrap"],
         image_hero: "https://res.cloudinary.com/dwptyupfa/image/upload/v1703065721/article/t6pzpp7wkk3ifpoqdt76.png"
